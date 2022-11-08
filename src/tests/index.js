ea32a3a771888e87d6e16ac746019a1cb1be9d2f
Silva Filho <tarusilva@gmail.com>
2022-11-08 18:27:26 -0300

    feat: cria arquivo de import básico dos testes
    Cria o arquivo de import básico dos testes:
    
    - cleanup, render, screen do "@testing-library/react"; e
    
    - userEvent do "@testing-library/user-event".

---
+++ src/tests/index.js
@@ -0,0 +1,9 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+export { 
+    cleanup,
+    render, 
+    screen, 
+    userEvent 
+};
